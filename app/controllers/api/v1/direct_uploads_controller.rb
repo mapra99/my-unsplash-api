2141ec7f4ea5fb835f64f0b489884a05fd162e4a
Miguel Prada <mapra99@gmail.com>
2021-03-28 22:28:16 -0500

    todos

--- app/controllers/api/v1/direct_uploads_controller.rb
+++ app/controllers/api/v1/direct_uploads_controller.rb
@@ -1,3 +1,4 @@
 class Api::V1::DirectUploadsController < ActiveStorage::DirectUploadsController
+  # TODO: Find a workaround for this!!
   protect_from_forgery except: :create
 end
--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,6 +1,6 @@
 module Types
   class PostType < Types::BaseObject
-    # field id: ID, null: false
+    field :id, ID, null: false
     field :title, String, null: false
     field :image_url, String, null: false
     field :created_at, String, null: false
