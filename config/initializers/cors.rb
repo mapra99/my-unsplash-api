e41bde19348728eb6dd70528f55cee52a2085d8e
Miguel Prada <mapra99@gmail.com>
2021-04-18 19:07:54 -0500

    trying another regexp

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -24,7 +24,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins '/\Ahttps?:\/\/(deploy-preview-.*)?my-unsplashh.netlify\.app/\z/'
+    origins '/\Ahttps?:\/\/my-unsplashh.netlify\.app/\z/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
