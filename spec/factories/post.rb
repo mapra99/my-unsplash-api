4e9193b2e9d04f4ced8adaaad05171bc8728e3fc
Miguel Prada <mapra99@gmail.com>
2021-05-06 02:17:32 -0500

    posts spec refactor

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :post do
     title { Faker::Movie.title }
-    image { Rack::Test::UploadedFile.new('spec/files/test-image.jpg', 'image/jpeg') }
+
+    factory :post_with_image do
+      image { Rack::Test::UploadedFile.new('spec/files/test-image.jpg', 'image/jpeg') }
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject { create(:post) }
-
   context 'associations' do
     it { should have_one_attached(:image) }
   end
@@ -12,6 +10,8 @@ RSpec.describe Post, type: :model do
   end
 
   describe '#image_url', skip_gh_actions: true do
+    subject { create(:post_with_image) }
+
     it 'returns a URL for the image' do
       image_url = subject.image_url
       expect(image_url).not_to be_empty
