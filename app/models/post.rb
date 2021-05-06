27589ac912d2bcc4754a138e13e758f938224448
Miguel Prada <mapra99@gmail.com>
2021-05-06 02:24:47 -0500

    Posts specs (#20)
    * post specs
    
    * s3 bucket for dev and test purposes
    
    * added tag to conveniently skip some specs on github actions
    
    * added env var to gh actions
    
    * posts spec refactor
    
    * skipped image attachment spec on github actions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,15 @@
 class Post < ApplicationRecord
+  include Paginable
+
   has_one_attached :image
 
   validates_presence_of :title
 
   scope :sorted_by_newest, -> { order(created_at: :desc) }
 
-  def self.paginate(limit, offset)
-    limit(limit).offset(offset)
-  end
-
   def image_url
     return '' unless image.attached?
 
-    image.service_url
+    image.url
   end
 end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context 'associations' do
+    example('image attachment', skip_gh_actions: true) do
+      should have_one_attached(:image)
+    end
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:title) }
+  end
+
+  describe '#image_url', skip_gh_actions: true do
+    subject { create(:post_with_image) }
+
+    it 'returns a URL for the image' do
+      image_url = subject.image_url
+      expect(image_url).not_to be_empty
+      expect(image_url).to match(URI::DEFAULT_PARSER.make_regexp)
+    end
+  end
+end
