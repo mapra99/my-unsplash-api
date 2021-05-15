fae03ab38224b84142d68d8c4f509ae8b470c726
Miguel Prada <mapra99@gmail.com>
2021-05-15 01:20:42 -0500

    devise init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Action mailer default url
+  config.action_mailer.default_url_options = { host: ENV.fetch("ACTION_MAILER_DEFAULT_HOST"), port: ENV.fetch("PORT") { 3000 } }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
