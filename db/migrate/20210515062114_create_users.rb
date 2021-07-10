ed03ed92cb84f7df272f78e88dd7915a621a6eee
Miguel Prada <mapra99@gmail.com>
2021-07-09 21:13:35 -0500

    update model with password and password confirmation

--- db/migrate/20210515062114_create_users.rb
+++ db/migrate/20210515062114_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :name, null: false
       t.string :email, null: false
       t.string :username, null: false
+      t.string :password, null: false
+      t.string :password_confirmation, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2021_05_15_062114) do
     t.string "name", null: false
     t.string "email", null: false
     t.string "username", null: false
+    t.string "password", null: false
+    t.string "password_confirmation", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
